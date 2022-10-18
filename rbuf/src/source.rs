f3e3c2fe1883d97762c8ce4894933e70195b9066
MrGunflame <mrgunflame@protonmail.com>
2022-10-18 12:51:05 +0200

    Refactor

--- rbuf/src/source.rs
+++ rbuf/src/source.rs
@@ -1,10 +1,11 @@
+use std::os::unix::prelude::AsRawFd;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 
 use futures::StreamExt;
 use gstreamer::{
-    element_error, element_warning, gst_warning, prelude::*, ClockTime, ElementFactory,
-    MessageView, Pipeline, State,
+    element_error, element_warning, gst_warning, prelude::*, ClockTime, Element, ElementFactory,
+    GenericFormattedValue, MessageView, Pipeline, State,
 };
 use gstreamer_app::AppSrc;
 
@@ -13,9 +14,9 @@ use crate::size::Size;
 
 // const URI: &str = "https://video-weaver.frahttps://video-edge-a2c4ec.pdx01.abs.hls.ttvnw.net/v1/segment/Cl7rg1JWnRNFDwCSxdo1DRdXxTrn7PV-QU-ZAYDzS3fz_JGvf1TvWyHCScbFLLocXCtCgQhHyTXXVIMLffX6-S6JbmEUC4wlJUlPZOChjPo2RV8nsVW6iLyMMI6HYudC4FKEXX1-EM7JHISto3QOlATYcG9ijggZYsFAPEbGltjPGpxNJgK01vB24rQK4apYg0keQ7TqOrxUFMOumUpmyVWysGn7XrZ_VV9hnIkA1Uq6OT_6raYWTN6F0tsIEE1HnHmzoW6RIn7LsQsaTSCPug9ou-fAqkn7PXsLqbDnojREU1SRi-YGFN7fqKJQhSEZx6wIPk5peUPK0keuwIVNrg2Jax6lpe4qxljxujONnPnU6WntX2djuTQxE24i8_N7dloX1BrbXpfnjH3UoW0Nu6Z3oNDoyyiVUATZQsUahMrkygutWhH2bQVWJobwlN3tExoOasnp_P196-POeIQAlBWYWSOmfUomkdr5-ATWHkOLouYCbc0Pr_ozM2IOADqzzyO8toxZYgbhX8Q0XuMOA-40TFPh-jQAWeATJD01UglNDbC1qSh0aMV1MAdGpa23U6Ca4IzWTS7SykbPyjXaaCBt7Fe9jhbso9bOc0d07Rk1EGmM6yknLM1dhfC2kU_zmgRI1Li3KREli0rMnaGaz8VwcGmM8SmjfNzq9xCdTqPr07wXnreWz1CPCyKZzgu32o8umFGTn0OJ3SuwIK8L41egWT2t8DfJ6UwMWnx7wYLN6aiZyYjT_sIdV6kdCGLHnSkDLS.ts05.hls.ttvnw.net/v1/playlist/CqkF01yHUUB4u3NiYBayoR0lqA4pk-WRkBi_sIxZ7Zu4BuQvfDMtQ_kNOILboXs3y7lSZ4D_HBSi2Vbo2Znfo273bjMiEXpTeQIht5ED_k-YZXWv5PrLdFzIRcVh_NZoo82wiF31Yk2ES5bplFbDjbiz-Cnph731pwdFx362KjwVhi4iEI_GyFpLKFPrReEq169AroEADUACC5b68k5h6eWbAC11z2U-2lAYse_uQrw3v-yphfQPm4ctMyLHX_d5yyPpq0rlRR36ZZnsULmedAsUWXPTg7v8igpwGU9E32twd46dGPvXDc7An4z3bJqE-65B0XMKI_HU4ybVKJVDT5eIYS4aAwZnRsKI5d-1D8dJ-TsLhyGYOBw2PzspyiA21BrSKJgLgtMHjGa-zAF8zEcJ0XwnpxByGgOlPEG1M8RbDW9W80nVQoLKL3qpol9xrgpY44ApeoUIBNgP_Y5jlFOQIj-RzrXafUrXeznb2Qk6O5Z5TCVpKXjBG5jZagNRnd2laaitofEJxyMuVb4qNaav3Tt5HdtWcJ0-RK2PFgNzB3NIYVRM6Wg4ThC6uk3inP3GvyWDUVlDDuY00dDuUHHyl4dXg5wBhdWO7PXw-F9JGELbhoTiJqvq16klTrtL-nBYHxOBUYtmdz6dG3dATE7EnfkAQroX6xJEbPBAAq3Gopt2Rp7CHT0NPjfZNa3bcHFXv_liO52Aj2WAFsJtmou1X5y8Bw-DkN7DmKfag-NPeTLxbTKftFCbNg7eqYYP14m9I3-fQgCZhHpmQb7Ck8OwrR0Jw7mKvpJZTZMpL7WgvsMeyaSbOCcnoPJUFqTy6Sd7BU07oxITyzQY1Ph3_2GiZJz1ke627gTtEJAfq_8k_vhQ6cdP7_G3Ub7arE3iWZmSSI3cqo5HQgttGgzZSV46X_Dhy9aK-oMgASoJZXUtd2VzdC0yMOsE.m3u8";
 // const URI:&str = "https://video-weaver.fra05.hls.ttvnw.net/v1/playlist/CrIFApzs6fCT1lJLhpOmmw2gR0FYLsMQzvaLQr_k41N9LGmI5E-3Sya2q7PX35eUJzR1Y3njJ-jVWnC4Y7VQj1D5DsOZj9WFKeKgwEjhat2gyPvUpbUNlCbG-YsY1ZQ_Ex9UiTJvpcfU1oP9ncnSataXtVeY4drXjeIlZiaYjd5ER-mDVHX6s_LHu4owf4_9iunDXt0Dga8id8DLupz_iYUJiOketiV818yFA2XDHyVMgxwatS4svqXv_WsyICStI1WTyWiH7jokZyp7ia4zQtNHnQr6x2vJn0UkcxnB0Wx-xuMOmOLPPa6o8BO_CeMbz_AWVhezpNwIecXs57dUi0yajByES6v3twbxV7tjfo08QFN-ewMqDkxgi8_Yn2IEgTXha4gVT-JEnCEPKRvN3fJZ9x2Ip_RWPxxF9_zKh5zuqvAA75LKXZRiaHcDyB1SjKAU-vq16uVa2KdAZbsD1ERpN-sZQUbqqDNX-lMzSqS4iX9SWyR9ISFm4SaVX7h4EUs5bIlm2TqR5_ammPCMBcvN0N0VqHrkzBxVwxAbs47LnJmq0DK9OodyGTMImC9aaSyI4MnNmsHPhbl8c2Nb05N7dI-EKWFwFd3PycQtgBb3HcchbPQBME8khxsBhIrXfjPHHATyvuypcxcrJXT0Gpgl0DQeS-4N7DHAdLc-gC3YWtJYhBzOhQGt4kA-D696WO2BhJqe9xhbmoXt9pgHjll0y7AvX7EMUQqtbYKDB2U_wgX-CC-Z00hSrJ1b1chtPjz3SNs1Xg_cGFQ1cLJO-3z_uo1C_HJJG2k2vHqVbdv0Opch5xJRhlA95H_jAk7I5AHkMb4Z7fELxlD7oAeX0HvGHdXMoY0q3OJRRxemKAzE49kq5-P-XHSWiDC-KSsMkvPJ0McPP_kuLx4XMjRAMJi_P2d9GgxhfPpW3A5_aQ4a_XcgASoJdXMtd2VzdC0yMOwE.m3u8";
-// const URI: &str = "http://devimages.apple.com/iphone/samples/bipbop/gear4/prog_index.m3u8";
+const URI: &str = "http://devimages.apple.com/iphone/samples/bipbop/gear4/prog_index.m3u8";
 // const URI: &str = "https://video-weaver.fra02.hls.ttvnw.net/v1/playlist/CpgFGa7wm2n7HeMy0VIdPTzeu04autpVsD0QeJj6RACGNwxqnyEcrxzMUquRa35VnLPVvqgHxy15sXNhSEA2za8fMs4b7zY-YfWv69bGEntaR3I6p-rKPs8eAZT4VuDmWcqHi-Fe9yJ_cCSSZM1bHsOlyXKK1_J8lNvhCIwvzwSoOc0TKisT_mhPYK6PS7ySIaTZE3Ap3hPD3KLGvhcnDCm7mz1nLPApraMPyPEqxiRNT5pD4FVRNvdizL_LPJlep-nHjiuRKqppNyY6bVK4Q_jE3XtkRj9r7lbon7OrmPRIgu8EeleBSZ946iyzKW9g5Xu8Ktsb41ySt0AzD8DNx4Ucd6ePqYKfRDNlPSVrl6t9QgNkXa9vNDHNnZKYVkYxAhuwrT6ce92qZHHxn2bE0mWn2A5crJ7AQEUQwhdfRrfdNqJImj24r-CzXe2TR-hMZl71faXOJ4nCD7R7q_EA2GSVW84itizI6KiK8rlD-HBZVMod4sglifOfW9A7cmohLiHQ9qOteSw_2p95PxMOoRZw6UMYzvK7BK9vbVQrZGi2MRF-nUojX8UpAPKt49K31zkx8oaDwRyYH55-Dz6u3AXR83Ja2oefxGG8SAOoQ6JpPHN05Qwr01oMduoZ9a7T8y_BlnH09O_L-5fIpaDfuBXifaHfk2RxxwMkPtMrMS3Dv6YCXiCHvbx75YMomwXccaqqMxf-EB3Yrvn3feyGt17axH19lj6zBezj1vRy0_HRja1RllECSMJA-drhksVKYz86KpzQFlQrmWncNqc1CUbHPtSY0AbA1LEVKTpla9m_Zo-dHjDNp4mZbAMJjqzHrxrEJIakXDoKT6WaO9n3bRVwYz6tvFKPN5aciSBnG83Vs6ILcHABfAezuBoMCa_nbYCj6tNcoYCEIAEqCWV1LXdlc3QtMjD0BA.m3u8";
-const URI: &str = "https://video-weaver.fra02.hls.ttvnw.net/v1/playlist/CqoFxrwx4iP7w-jEztUwjKYFfvOqmhGuMXuxSF5_7dVgEmIRDT9SzZyvSkR2cfsazzCMtBGPbhz8C37JsDA8fnZZ0G-u4XntR0qB39koarahdVSXbnk0IHTFUS6oDlTo26sVnH-KDTNejGheBlAoQ5mWD_jn_waMXy_Sb2uq-n12VYiPz4jArePyot-2cicXsScKOyR1gkyovojaPhCzxoVVUbbYYKXSVBj4SQoC7EnvM9eVL6waqfJlquzAO9Ozmw4zS56YoSIlZph4A9dSG4YXQGsgHlhW-nU_6fATN9iC9FS7fdmUkCZu6OSjoQMOXSvxhmmzbTgyl9YEEden_t4OO-Yvlx9KxyS4BEa5ZYeb78VznhW3Vo_kid_M-CQTkFjX6Rq-G_ZnBwulbcdNijOeX9BF23AiICYOG3FeX1IBlDWQt4M94PQ4SmsNkkTmb6Y4ylsQs_4bqxW4_T5VlPHmg1rtBkWm3KcfQIv_kZ2aIHX0lkfb_1pb3dKzN8MPafYyTL7mr7vwtupCMeUOrq_2AU0El5zzx7M23Xf-JMNrFrm69ZRGM9AkwuGNAZKTjD2QLzx_mT5t75hJCZ8kMz6sd5TX34y-Xork1K0kjF6umqsTqskYlJB2KGz_48Tt-g6VDqVf9VfBnkHo-Gcm3T5ii5PTArVFz9IDo6cf5VKaGbo2qOLKadCuWwD7WxsyJHOl_S-SNEakZEml3r1qhALdn8OiNu33s1ndWmeS0CIAB3lYVz4bnTYxRTNoP7BSVdNhqy_QpbxnhdpdEIkTFjtxHYv25Oxgp7Sz6odsz2yo-asxI-U-ewfVT465pLTKkkGhJn3Lvl2OY3QzFsbII_LstKvTUuBL48XmfYG8mxEIOcShwXy3Lx2kFmOIcop1SQpDFyVaQEt04qY0dxoMUqbMaXt7sLERHdtcIAEqCWV1LXdlc3QtMjD3BA.m3u8";
+// const URI: &str = "https://video-weaver.fra02.hls.ttvnw.net/v1/playlist/CqoFxrwx4iP7w-jEztUwjKYFfvOqmhGuMXuxSF5_7dVgEmIRDT9SzZyvSkR2cfsazzCMtBGPbhz8C37JsDA8fnZZ0G-u4XntR0qB39koarahdVSXbnk0IHTFUS6oDlTo26sVnH-KDTNejGheBlAoQ5mWD_jn_waMXy_Sb2uq-n12VYiPz4jArePyot-2cicXsScKOyR1gkyovojaPhCzxoVVUbbYYKXSVBj4SQoC7EnvM9eVL6waqfJlquzAO9Ozmw4zS56YoSIlZph4A9dSG4YXQGsgHlhW-nU_6fATN9iC9FS7fdmUkCZu6OSjoQMOXSvxhmmzbTgyl9YEEden_t4OO-Yvlx9KxyS4BEa5ZYeb78VznhW3Vo_kid_M-CQTkFjX6Rq-G_ZnBwulbcdNijOeX9BF23AiICYOG3FeX1IBlDWQt4M94PQ4SmsNkkTmb6Y4ylsQs_4bqxW4_T5VlPHmg1rtBkWm3KcfQIv_kZ2aIHX0lkfb_1pb3dKzN8MPafYyTL7mr7vwtupCMeUOrq_2AU0El5zzx7M23Xf-JMNrFrm69ZRGM9AkwuGNAZKTjD2QLzx_mT5t75hJCZ8kMz6sd5TX34y-Xork1K0kjF6umqsTqskYlJB2KGz_48Tt-g6VDqVf9VfBnkHo-Gcm3T5ii5PTArVFz9IDo6cf5VKaGbo2qOLKadCuWwD7WxsyJHOl_S-SNEakZEml3r1qhALdn8OiNu33s1ndWmeS0CIAB3lYVz4bnTYxRTNoP7BSVdNhqy_QpbxnhdpdEIkTFjtxHYv25Oxgp7Sz6odsz2yo-asxI-U-ewfVT465pLTKkkGhJn3Lvl2OY3QzFsbII_LstKvTUuBL48XmfYG8mxEIOcShwXy3Lx2kFmOIcop1SQpDFyVaQEt04qY0dxoMUqbMaXt7sLERHdtcIAEqCWV1LXdlc3QtMjD3BA.m3u8";
 
 // -----------------------
 // -- FIXME: TWITCH BUG --
@@ -33,26 +34,17 @@ fn create_pipeline(buf: Arc<VideoBuffer>) -> Pipeline {
     // let src = gstreamer::ElementFactory::make("filesrc", None).unwrap();
     // src.set_property("location", super::PATH2);
 
-    let queue = ElementFactory::make("multiqueue", None).unwrap();
-    queue.set_property("max-size-buffers", 0u32);
-    queue.set_property("max-size-time", 0u64);
-    queue.set_property("max-size-bytes", 1024u32 * 1024 * 100);
-
-    let hlsdemux = gstreamer::ElementFactory::make("hlsdemux", None).unwrap();
-
-    pipeline.add_many(&[&src, &queue, &hlsdemux]).unwrap();
-
-    src.link(&hlsdemux).unwrap();
-    // pipeline.add(&src).unwrap();
-
+    let hlsdemux = HlsDemux::new();
     let decodebin = DecodeBin::new(&pipeline, buf);
 
-    let muxer_clone = decodebin.clone();
-    let queue_clone = queue.clone();
-    hlsdemux.connect_pad_added(move |demux, src_pad| {
-        handle_demux_pad_added(demux, src_pad, &queue_clone, &muxer_clone.element.as_ref());
-    });
-    hlsdemux.sync_state_with_parent().unwrap();
+    pipeline
+        .add_many(&[&src, &hlsdemux.element, &decodebin.element])
+        .unwrap();
+
+    src.link(&hlsdemux.element).unwrap();
+    // pipeline.add(&src).unwrap();
+
+    hlsdemux.build(decodebin);
 
     // src.link(&decodebin.element).unwrap();
 
@@ -92,7 +84,6 @@ pub struct DecodeBin {
 impl DecodeBin {
     pub fn new(pipeline: &Pipeline, buf: Arc<VideoBuffer>) -> Self {
         let element = ElementFactory::make("decodebin", None).unwrap();
-        pipeline.add(&element).unwrap();
 
         let pipeline = pipeline.downgrade();
         let buf_clone = buf.clone();
@@ -130,49 +121,72 @@ impl DecodeBin {
             };
 
             if is_audio {
-                // let queue = ElementFactory::make("queue", None).unwrap();
-                // let convert = ElementFactory::make("audioconvert", None).unwrap();
-                // let resample = ElementFactory::make("audioresample", None).unwrap();
-                // let sink = ElementFactory::make("autoaudiosink", None).unwrap();
-                // let sink = AppSink::new(buf);
-                // let sink_el: &gstreamer::Element = sink.element.as_ref();
-
-                // // //let elements = [&queue, &convert, &resample, &sink];
-                // let elements = [sink_el];
-                // pipeline.add_many(&elements).unwrap();
-                // gstreamer::Element::link_many(&elements).unwrap();
-
-                // for e in elements {
-                //     e.sync_state_with_parent().unwrap();
-                // }
-
-                // let sink_pad = sink_el.static_pad("sink").expect("queue has no sinkpad");
-                // src_pad.link(&sink_pad).unwrap();
-            } else if is_video {
-                buf.set_caps(src_pad.caps().unwrap());
-
-                // let queue = ElementFactory::make("queue", None).unwrap();
-                // let convert = ElementFactory::make("videoconvert", None).unwrap();
-                // let scale = ElementFactory::make("videoscale", None).unwrap();
-                // let sink = ElementFactory::make("autovideosink", None).unwrap();
-                let sink = AppSink::new(buf);
-                let sink_el: &gstreamer::Element = sink.element.as_ref();
-
-                // let elements = [&queue, &convert, &scale, &sink];
-                let elements = [sink_el];
-                pipeline.add_many(&elements).unwrap();
-                //gstreamer::Element::link_many(&[&queue, &convert, &scale]).unwrap();
-
-                for e in elements {
-                    e.sync_state_with_parent().unwrap();
-                }
-
-                let sink_pad = sink_el.static_pad("sink").expect("queue has no sinkpad");
-                src_pad.link(&sink_pad).unwrap();
+                return;
             }
+
+            let sink = AppSink::new(buf);
+            let sink_el: &gstreamer::Element = sink.element.as_ref();
+            let elements = [sink_el];
+            pipeline.add_many(&elements).unwrap();
+            //gstreamer::Element::link_many(&[&queue, &convert, &scale]).unwrap();
+
+            for e in elements {
+                e.sync_state_with_parent().unwrap();
+            }
+
+            let sink_pad = sink_el.static_pad("sink").expect("queue has no sinkpad");
+            src_pad.link(&sink_pad).unwrap();
+
+            // if is_audio {
+            //     // let queue = ElementFactory::make("queue", None).unwrap();
+            //     // let convert = ElementFactory::make("audioconvert", None).unwrap();
+            //     // let resample = ElementFactory::make("audioresample", None).unwrap();
+            //     // let sink = ElementFactory::make("autoaudiosink", None).unwrap();
+            //     // let sink = AppSink::new(buf);
+            //     // let sink_el: &gstreamer::Element = sink.element.as_ref();
+
+            //     // // //let elements = [&queue, &convert, &resample, &sink];
+            //     // let elements = [sink_el];
+            //     // pipeline.add_many(&elements).unwrap();
+            //     // gstreamer::Element::link_many(&elements).unwrap();
+
+            //     // for e in elements {
+            //     //     e.sync_state_with_parent().unwrap();
+            //     // }
+
+            //     // let sink_pad = sink_el.static_pad("sink").expect("queue has no sinkpad");
+            //     // src_pad.link(&sink_pad).unwrap();
+            // } else if is_video {
+            //     buf.set_caps(src_pad.caps().unwrap());
+
+            //     // let queue = ElementFactory::make("queue", None).unwrap();
+            //     // let convert = ElementFactory::make("videoconvert", None).unwrap();
+            //     // let scale = ElementFactory::make("videoscale", None).unwrap();
+            //     // let sink = ElementFactory::make("autovideosink", None).unwrap();
+            //     let sink = AppSink::new(buf);
+            //     let sink_el: &gstreamer::Element = sink.element.as_ref();
+
+            //     // let elements = [&queue, &convert, &scale, &sink];
+            //     let elements = [sink_el];
+            //     pipeline.add_many(&elements).unwrap();
+            //     //gstreamer::Element::link_many(&[&queue, &convert, &scale]).unwrap();
+
+            //     for e in elements {
+            //         e.sync_state_with_parent().unwrap();
+            //     }
+
+            //     let sink_pad = sink_el.static_pad("sink").expect("queue has no sinkpad");
+            //     src_pad.link(&sink_pad).unwrap();
+            // }
         });
 
         Self { element }
+    }
+}
+
+impl AsRef<Element> for DecodeBin {
+    fn as_ref(&self) -> &Element {
+        &self.element
     }
 }
 
@@ -182,11 +196,28 @@ pub struct HlsDemux {
 }
 
 impl HlsDemux {
-    // pub fn new(pipeline: &Pipeline) -> Self {
-    //     let hlsdemux = ElementFactory::make("hlsdemux", None).unwrap();
+    pub fn new() -> Self {
+        let hlsdemux = ElementFactory::make("hlsdemux", None).unwrap();
 
-    //     element.connect_pad_added(move |demux, src_pad| {});
-    // }
+        Self { element: hlsdemux }
+    }
+
+    pub fn build(&self, sink: DecodeBin) {
+        self.element.connect_pad_added(move |demuxer, src_pad| {
+            tracing::debug!("[HLSDEMUX] handle_demux_pad_added");
+
+            let sink_pad = sink.element.static_pad("sink").unwrap();
+            src_pad.link(&sink_pad).unwrap();
+        });
+
+        self.element.sync_state_with_parent().unwrap();
+    }
+}
+
+impl AsRef<Element> for HlsDemux {
+    fn as_ref(&self) -> &Element {
+        &self.element
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -380,4 +411,85 @@ async fn pipline_run_async(pipeline: &Pipeline) {
     }
 
     pipeline.set_state(State::Null).unwrap();
+}
+
+trait Hls {}
+
+pub trait Sink<T>: AsRef<Element> {}
+
+pub trait Source<T>: AsRef<Element> {
+    fn link<S>(&self, sink: &S)
+    where
+        S: Sink<T>,
+    {
+        // TODO: error handling
+        self.as_ref().link(sink.as_ref()).unwrap();
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct AutoVideoSink {
+    element: Element,
+}
+
+impl AutoVideoSink {
+    pub fn new() -> Self {
+        let element = ElementFactory::make("autovideosink", None).unwrap();
+
+        Self { element }
+    }
+}
+
+impl AsRef<Element> for AutoVideoSink {
+    fn as_ref(&self) -> &Element {
+        &self.element
+    }
+}
+
+impl Sink<PcmVideo> for AutoVideoSink {}
+
+pub trait MediaFormat {
+    /// Returns `true` if the format encapsulates an audio stream.
+    fn is_audio(&self) -> bool;
+
+    /// Returns `true` if the format encapsulates a video stream.
+    fn is_video(&self) -> bool;
+}
+
+pub trait AudioMediaFormat: MediaFormat {}
+
+pub trait VideoMediaFormat: MediaFormat {}
+
+/// Any raw PCM video format (e.g. I420 YUV).
+pub struct PcmVideo {}
+
+impl MediaFormat for PcmVideo {
+    fn is_audio(&self) -> bool {
+        false
+    }
+
+    fn is_video(&self) -> bool {
+        true
+    }
+}
+
+impl VideoMediaFormat for PcmVideo {}
+
+/// Any raw PCM audio format (e.g. F32LE).
+pub struct PcmAudio {}
+
+impl MediaFormat for PcmAudio {
+    fn is_audio(&self) -> bool {
+        true
+    }
+
+    fn is_video(&self) -> bool {
+        false
+    }
+}
+
+impl AudioMediaFormat for PcmAudio {}
+
+pub trait GstElement {
+    const PACKAGE: &'static str;
 }

bfb6bed05d12eff3d68a064ba7ea7c05dffcb01e
MrGunflame <mrgunflame@protonmail.com>
2022-11-02 14:30:23 +0100

    feat: Remove state::Connection

--- stsync-proxy/src/srt/state.rs
+++ stsync-proxy/src/srt/state.rs
@@ -7,7 +7,7 @@ use std::sync::atomic::{AtomicU32, AtomicU64, AtomicU8, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
-use ahash::{AHashMap, AHashSet};
+use ahash::AHashMap;
 use futures::StreamExt;
 use parking_lot::{Mutex, RwLock};
 use rand::rngs::OsRng;
@@ -22,7 +22,6 @@ use crate::srt::PacketType;
 use super::config::Config;
 use super::conn::AckQueue;
 use super::metrics::ConnectionMetrics;
-use super::sink::OutputSink;
 use super::{DataPacket, Header, IsPacket, Packet};
 
 #[derive(Debug)]
@@ -143,177 +142,6 @@ impl ConnectionPool {
         None
     }
 }
-
-/// All SRT traffic flows over a single UDP socket. A [`Connection`] represents a single stream
-/// to a peer multiplexed over that UDP socket.
-///
-/// On the initial handshake the client and server will exchange their [`SocketId`]s. When
-/// receiving a packet it must be matched against the `server_socket_id` to find an already
-/// existing stream. When sending a packet it must be sent to the `client_socket_id`.
-#[derive(Debug)]
-pub struct Connection {
-    pub id: ConnectionId,
-
-    /// Starting time of the connection. Used to calculate packet timestamps.
-    pub start_time: Instant,
-    pub server_socket_id: SocketId,
-    /// Socket id of the the remote peer.
-    pub client_socket_id: SocketId,
-    pub client_sequence_number: AtomicU32,
-    pub server_sequence_number: AtomicU32,
-
-    pub rtt: Rtt,
-    pub state: ConnectionState,
-    // TODO: Merge with state.
-    pub syn_cookie: AtomicU32,
-
-    pub inflight_acks: Mutex<AckQueue>,
-    pub lost_packets: Mutex<Vec<u32>>,
-
-    /// Total packets/bytes received from the peer.
-    // TODO: Merge together into single cell
-    pub packets_recv: AtomicU32,
-    pub bytes_recv: AtomicU32,
-
-    pub mode: Mutex<ConnectionMode>,
-    /// Relative timestamp of the last transmitted message. We drop the connection when this
-    /// reaches 5s.
-    pub last_packet_time: Mutex<Instant>,
-    pub metrics: ConnectionMetrics,
-
-    pub shutdown: Notify,
-
-    pub tx: mpsc::Sender<DataPacket>,
-    pub rx: Mutex<Option<mpsc::Receiver<DataPacket>>>,
-
-    /// The number of free buffers avaliable. Defaults to 8192.
-    pub buffers_avail: AtomicU32,
-    /// Async waker that should be called if a buffer comes avaliable after it reached 0.
-    pub buffers_waker: Notify,
-}
-
-impl Connection {
-    pub fn timestamp(&self) -> u32 {
-        Instant::now().duration_since(self.start_time).as_micros() as u32
-    }
-
-    pub fn delivery_time<T>(&self, header: T) -> Instant
-    where
-        T: AsRef<Header>,
-    {
-        let time = Duration::from_micros((header.as_ref().timestamp + self.rtt.load().0) as u64);
-        self.start_time + time
-    }
-
-    pub async fn send(&self, packet: DataPacket) {
-        let _ = self.tx.send(packet).await;
-    }
-
-    pub fn close(&self) {
-        self.shutdown.notify_one();
-    }
-
-    pub fn spawn_request<S>(
-        self: &Arc<Self>,
-        socket: Arc<UdpSocket>,
-        state: &State<S>,
-        resource_id: ResourceId,
-    ) where
-        S: SessionManager,
-    {
-        let mut stream = match state.session_manager.request(resource_id) {
-            Ok(s) => s,
-            Err(_) => return,
-        };
-
-        let conn = self.clone();
-
-        tokio::task::spawn(async move {
-            let mut msgnum = 1;
-
-            let shutdown = conn.shutdown.notified();
-
-            let fut = async {
-                while let Some(buf) = stream.next().await {
-                    // Wait for more buffers to become available.
-                    // if conn
-                    //     .buffers_avail
-                    //     .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |n| {
-                    //         if n == 0 {
-                    //             None
-                    //         } else {
-                    //             Some(n - 1)
-                    //         }
-                    //     })
-                    //     .is_err()
-                    // {
-                    //     tracing::info!("Buffering");
-                    //     conn.buffers_waker.notified().await;
-                    // }
-
-                    let seqnum = conn.server_sequence_number.fetch_add(1, Ordering::AcqRel);
-
-                    let mut packet = DataPacket::default();
-                    packet.header.set_packet_type(PacketType::Data);
-                    packet.header.timestamp = conn.timestamp();
-                    packet.header.destination_socket_id = conn.client_socket_id.0;
-                    packet.header().set_packet_sequence_number(seqnum);
-                    packet.header().set_message_number(msgnum);
-                    packet.header().set_ordered(true);
-                    packet
-                        .header()
-                        .set_packet_position(crate::srt::PacketPosition::Full);
-                    packet.data = buf.into();
-
-                    let addr = conn.id.addr;
-                    socket
-                        .send_to(&packet.upcast().encode_to_vec().unwrap(), addr)
-                        .await
-                        .unwrap();
-
-                    msgnum += 1;
-                }
-            };
-
-            tokio::select! {
-                _ = fut => {}
-                _ = shutdown => {}
-            }
-        });
-    }
-}
-
-impl<'a> Borrow<ConnectionId> for Arc<Connection> {
-    fn borrow(&self) -> &ConnectionId {
-        &self.id
-    }
-}
-
-impl Borrow<ConnectionId> for Connection {
-    fn borrow(&self) -> &ConnectionId {
-        &self.id
-    }
-}
-
-impl PartialEq<ConnectionId> for Connection {
-    fn eq(&self, other: &ConnectionId) -> bool {
-        self.id == *other
-    }
-}
-
-impl PartialEq for Connection {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-
-impl Hash for Connection {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-    }
-}
-
-impl Eq for Connection {}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SocketId(pub u32);

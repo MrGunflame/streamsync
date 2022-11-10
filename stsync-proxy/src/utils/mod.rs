5126b1c41abcf5a023b177849c08f6cc8c49947f
MrGunflame <mrgunflame@protonmail.com>
2022-11-10 15:50:33 +0100

    fix: Implement Shared<T> traits independent of T

--- stsync-proxy/src/utils/mod.rs
+++ stsync-proxy/src/utils/mod.rs
@@ -1,7 +1,9 @@
+use std::cmp::Ordering;
+use std::fmt::{self, Debug, Formatter};
+use std::hash::{Hash, Hasher};
 use std::ptr::NonNull;
 
 /// A shared, raw pointer of `T`. `Shared` is a covariant over `T`.
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct Shared<T>(NonNull<T>);
 
@@ -18,6 +20,47 @@ impl<T> From<&T> for Shared<T> {
     #[inline]
     fn from(src: &T) -> Self {
         Self(src.into())
+    }
+}
+
+impl<T> Clone for Shared<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for Shared<T> {}
+
+impl<T> Debug for Shared<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T> PartialEq for Shared<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<T> Eq for Shared<T> {}
+
+impl<T> PartialOrd for Shared<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<T> Ord for Shared<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl<T> Hash for Shared<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
     }
 }
 

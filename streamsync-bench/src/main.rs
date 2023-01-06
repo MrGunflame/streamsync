80b4c96936e5c5152db31cc1589bbd3230816418
MrGunflame <mrgunflame@protonmail.com>
2023-01-06 14:53:17 +0100

    Replicate original timestamps for retransmitted frames

--- streamsync-bench/src/main.rs
+++ streamsync-bench/src/main.rs
@@ -1,20 +1,25 @@
 use std::process::Command;
 
+use clap::Parser;
+
 use streamsync_api::v1::Session;
 use streamsync_api::Client;
 
 #[tokio::main]
 async fn main() {
-    let client = Client::new("http://127.0.0.1:9998");
+    let args = Args::parse();
+
+    let client = Client::new(format!("http://{}:9998", args.host));
     client.authorize("test".to_owned());
 
-    for i in 1..=5 {
+    for i in 1..=args.number {
         let res = Session::create(&client, i.to_string()).await.unwrap();
 
         let opts = Options {
-            host: "127.0.0.1:9999".into(),
+            host: args.host.clone(),
             resource_id: res.resource_id,
             session_id: res.session_id,
+            file: args.input.clone(),
         };
 
         std::thread::spawn(move || {
@@ -23,9 +28,10 @@ async fn main() {
 
         let res = Session::create(&client, i.to_string()).await.unwrap();
         let opts = Options {
-            host: "127.0.0.1:9999".into(),
+            host: args.host.clone(),
             resource_id: res.resource_id,
             session_id: res.session_id,
+            file: args.input.clone(),
         };
 
         std::thread::spawn(move || {
@@ -36,24 +42,36 @@ async fn main() {
     loop {}
 }
 
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, short = 'u')]
+    host: String,
+    #[arg(long, short = 't')]
+    token: String,
+    #[arg(long, short = 'i')]
+    input: String,
+    #[arg(long, short = 'n')]
+    number: u8,
+}
+
 #[derive(Clone, Debug)]
 struct Options {
     host: String,
     resource_id: String,
     session_id: String,
+    file: String,
 }
 
 fn start_publish(opts: &Options) {
     let addr = format!(
-        "srt://{}?streamid=#!::m=publish,r={},s={}",
+        "srt://{}:9999?streamid=#!::m=publish,r={},s={}",
         opts.host, opts.resource_id, opts.session_id
     );
 
-    let file = "test.ts";
-
     let cmd = Command::new("ffmpeg")
         .args([
-            "-re", "-i", file, "-acodec", "copy", "-vcodec", "copy", "-f", "mpegts", &addr,
+            "-re", "-i", &opts.file, "-acodec", "copy", "-vcodec", "copy", "-f", "mpegts", &addr,
         ])
         .spawn()
         .unwrap();
@@ -61,7 +79,7 @@ fn start_publish(opts: &Options) {
 
 fn start_request(opts: &Options) {
     let addr = format!(
-        "srt://{}?streamid=#!::m=request,r={},s={}",
+        "srt://{}:9999?streamid=#!::m=request,r={},s={}",
         opts.host, opts.resource_id, opts.session_id
     );
 

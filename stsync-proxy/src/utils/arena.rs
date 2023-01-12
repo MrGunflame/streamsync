a8e7a9ed28bae2ec7c55f0131366c29897a82757
MrGunflame <mrgunflame@protonmail.com>
2023-01-12 21:52:02 +0100

    Cleanup and more docs

--- stsync-proxy/src/utils/arena.rs
+++ stsync-proxy/src/utils/arena.rs
@@ -7,31 +7,48 @@ use std::slice;
 use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 
 use bytes::{Bytes, Vtable};
-use parking_lot::Mutex;
+use parking_lot::RwLock;
 
-const PAGE: usize = 65536;
+/// The default chunk size.
+pub const PAGE: usize = 65536;
 
+/// A fast arena-based byte allocator optimized for short lifetime objects.
+///
+/// `Arena` is optimized for objects with a short lifetime, e.g. network buffers. The allocator
+/// implementation is not stabilized and might change in the future.
+///
+/// The current implementation is based on a thread-shared bump allocator with reusable chunks.
 #[derive(Debug)]
 pub struct Arena {
     chunk_size: usize,
     // FIXME: Maybe this can be a lock-free intrusive doubly linked list.
-    chunks: Mutex<Vec<Chunk>>,
+    chunks: RwLock<Vec<Chunk>>,
+    max_chunks: usize,
 }
 
 impl Arena {
+    /// Creates a new `Arena` with the given `chunk_size`.
+    #[inline]
     pub fn new(chunk_size: usize) -> Self {
         Self {
             chunk_size,
-            chunks: Mutex::new(Vec::new()),
+            chunks: RwLock::new(Vec::new()),
+            max_chunks: usize::MAX,
         }
     }
 
+    #[inline]
+    pub const fn builder() -> ArenaBuilder {
+        ArenaBuilder::new()
+    }
+
+    /// Acquires a new, mutable memory block from `Arena` and returns it in form of a [`BytesMut`].
     pub fn get(&self, size: usize) -> BytesMut {
         if size > self.chunk_size {
             panic!("Cannot allocate larger than chunk size");
         }
 
-        let mut chunks = self.chunks.lock();
+        let chunks = self.chunks.read();
 
         for chunk in &*chunks {
             // We have the only pointer to the chunk. The chunk allocation
@@ -47,15 +64,23 @@ impl Arena {
             }
         }
 
+        if chunks.len() >= self.max_chunks {
+            panic!("Arena chunks oom");
+        }
+
+        drop(chunks);
         let chunk = Chunk::new(self.chunk_size);
         let buf = chunk.get(size).unwrap();
+
+        let mut chunks = self.chunks.write();
         chunks.push(chunk);
 
         buf
     }
 
+    #[inline]
     pub fn chunks(&self) -> usize {
-        let chunks = self.chunks.lock();
+        let chunks = self.chunks.read();
         chunks.len()
     }
 }
@@ -67,6 +92,63 @@ impl Default for Arena {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
+pub struct ArenaBuilder {
+    chunk_size: usize,
+    min_chunks: usize,
+    max_chunks: usize,
+}
+
+impl ArenaBuilder {
+    #[inline]
+    pub const fn new() -> Self {
+        Self {
+            chunk_size: PAGE,
+            min_chunks: 0,
+            max_chunks: usize::MAX,
+        }
+    }
+
+    #[inline]
+    pub const fn chunk_size(mut self, chunk_size: usize) -> Self {
+        self.chunk_size = chunk_size;
+        self
+    }
+
+    #[inline]
+    pub const fn min_chunks(mut self, min_chunks: usize) -> Self {
+        self.min_chunks = min_chunks;
+        self
+    }
+
+    #[inline]
+    pub const fn max_chunks(mut self, max_chunks: usize) -> Self {
+        self.max_chunks = max_chunks;
+        self
+    }
+
+    pub fn build(self) -> Arena {
+        let mut chunks = Vec::with_capacity(self.min_chunks);
+        for _ in 0..self.min_chunks {
+            chunks.push(Chunk::new(self.chunk_size));
+        }
+
+        Arena {
+            chunk_size: self.chunk_size,
+            chunks: RwLock::new(chunks),
+            max_chunks: self.max_chunks,
+        }
+    }
+}
+
+impl Default for ArenaBuilder {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A fixed-size mutable continguous slice of memory.
 #[derive(Debug)]
 pub struct BytesMut {
     chunk: NonNull<ChunkInner>,
@@ -75,12 +157,18 @@ pub struct BytesMut {
 }
 
 impl BytesMut {
+    /// Shortens the length of the buffer to `len`.
+    ///
+    /// If `len` is greater than the current length of the buffer, this has no effect.
+    #[inline]
     pub fn truncate(&mut self, len: usize) {
         if len <= self.len() {
             unsafe { self.set_len(len) };
         }
     }
 
+    /// Converts `self` into an immutable [`Bytes`].
+    #[inline]
     pub fn freeze(self) -> Bytes {
         let ptr = self.ptr.as_ptr();
         let len = self.len;
@@ -92,7 +180,8 @@ impl BytesMut {
         unsafe { Bytes::with_vtable(ptr, len, data, &SHARED_VTABLE) }
     }
 
-    pub unsafe fn set_len(&mut self, len: usize) {
+    #[inline]
+    unsafe fn set_len(&mut self, len: usize) {
         self.len = len;
     }
 
@@ -108,12 +197,14 @@ impl BytesMut {
 }
 
 impl AsRef<[u8]> for BytesMut {
+    #[inline]
     fn as_ref(&self) -> &[u8] {
         self.as_slice()
     }
 }
 
 impl AsMut<[u8]> for BytesMut {
+    #[inline]
     fn as_mut(&mut self) -> &mut [u8] {
         self.as_slice_mut()
     }
@@ -160,7 +251,7 @@ struct Chunk {
 
 impl Chunk {
     pub fn new(size: usize) -> Self {
-        let mut inner = Box::new(ChunkInner::new(size));
+        let inner = Box::new(ChunkInner::new(size));
         let ptr = Box::leak(inner).into();
 
         Self { ptr }
@@ -314,224 +405,6 @@ unsafe fn chunk_drop(data: &mut AtomicPtr<()>, ptr: *const u8, len: usize) {
 
     unsafe { Box::from_raw(shared) };
 }
-
-// #[derive(Debug)]
-// struct ArenaInner {
-//     page_size: usize,
-//     chunks: RwLock<Vec<(Chunk, *const u8)>>,
-// }
-
-// impl Arena {
-//     #[inline]
-//     pub fn new(page_size: usize) -> Self {
-//         Self {
-//             inner: Arc::new(ArenaInner {
-//                 page_size,
-//                 chunks: RwLock::new(Vec::new()),
-//             }),
-//         }
-//     }
-
-//     unsafe fn alloc(&self, mut layout: Layout) -> NonNull<[u8]> {
-//         layout = layout.pad_to_align();
-
-//         if layout.size() > self.inner.page_size {
-//             panic!("exceeded page size");
-//             // The requested layout is bigger than the maximum chunk size. We can never
-//             // allocate this object with this arena. Forward to global allocator.
-//             // return unsafe { NonNull::new_unchecked(std::alloc::alloc(layout)) };
-//             // Equivalent to ptr.cast() but is unsized.
-//         }
-
-//         let chunks = self.inner.chunks.read();
-//         for (chunk, _) in &*chunks {
-//             if let Ok(ptr) = unsafe { chunk.allocate_padded(layout) } {
-//                 return ptr;
-//             }
-//         }
-
-//         let (chunk, chunk_ptr) = self.make_chunk();
-
-//         drop(chunks);
-//         let mut chunks = self.inner.chunks.write();
-//         let ptr = unsafe { chunk.allocate_padded(layout).unwrap() };
-
-//         chunks.push((chunk, chunk_ptr));
-//         ptr
-//     }
-
-//     unsafe fn dealloc(&self, ptr: NonNull<u8>, layout: Layout) {
-//         let mut chunks = self.inner.chunks.write();
-//         for (i, (chunk, base)) in chunks.iter().enumerate() {
-//             // Pointer within base + page_size; the pointer is a part of this chunk.
-//             if ptr.as_ptr() as usize <= *base as usize + self.inner.page_size {
-//                 // SAFETY: `ptr` was previously allocated in this `chunk`.
-//                 unsafe {
-//                     chunk.deallocate(layout, ptr);
-//                 }
-
-//                 chunks.remove(i);
-//                 break;
-//             }
-//         }
-//     }
-
-//     #[inline]
-//     fn make_chunk(&self) -> (Chunk, *const u8) {
-//         let layout = unsafe {
-//             Layout::from_size_align_unchecked(
-//                 std::mem::size_of::<u8>() * self.inner.page_size,
-//                 std::mem::align_of::<usize>(),
-//             )
-//         };
-
-//         let ptr = unsafe { std::alloc::alloc(layout) };
-//         assert!(!ptr.is_null());
-
-//         let ptr = unsafe { NonNull::new_unchecked(ptr) };
-
-//         (
-//             Chunk {
-//                 arena: self.clone(),
-//                 ptr,
-//                 layout,
-//                 head: AtomicUsize::new(0),
-//                 entries: AtomicUsize::new(0),
-//             },
-//             ptr.as_ptr(),
-//         )
-//     }
-// }
-
-// impl Default for Arena {
-//     #[inline]
-//     fn default() -> Self {
-//         Self::new(PAGE)
-//     }
-// }
-
-// unsafe impl Allocator for Arena {
-//     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
-//         unsafe { Ok(self.alloc(layout)) }
-//     }
-
-//     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-//         unsafe { self.dealloc(ptr, layout) };
-//     }
-// }
-
-// /// A fixed size memory page.
-// #[derive(Debug)]
-// struct Chunk {
-//     arena: Arena,
-//     ptr: NonNull<u8>,
-//     layout: Layout,
-//     head: AtomicUsize,
-//     /// The number of active references to the buffer of this chunk.
-//     entries: AtomicUsize,
-// }
-
-// impl Chunk {
-//     /// # Safety
-//     ///
-//     /// The `Chunk` must have enough [`contiguous_capacity`] to allocate using `layout`.
-//     ///
-//     /// [`contiguous_capacity`]: Self::contiguous_capacity
-//     unsafe fn allocate_padded(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-//         // Acquire the head offset.
-//         let mut head = self.head.load(Ordering::SeqCst);
-//         if head > head + layout.size() {
-//             return Err(AllocError);
-//         }
-
-//         while let Err(current) = self.head.compare_exchange_weak(
-//             head,
-//             head + layout.size(),
-//             Ordering::SeqCst,
-//             Ordering::SeqCst,
-//         ) {
-//             head = current;
-
-//             // Another thread won the race and we no longer have enough space to
-//             // allocate using this `layout`.
-//             if head > head + layout.size() {
-//                 return Err(AllocError);
-//             }
-//         }
-
-//         self.entries.fetch_add(1, Ordering::SeqCst);
-
-//         let ptr = unsafe { NonNull::new_unchecked(self.ptr.as_ptr().add(head)) };
-//         let len = layout.size();
-
-//         // SAFETY: The caller guarantees that `end < buffer.len()`.
-//         // let fatptr = [self.buffer.as_ptr() as usize + head, layout.size()];
-//         // Ok(unsafe { std::mem::transmute::<[usize; 2], NonNull<[u8]>>(fatptr) })
-//         // Ok(unsafe { self.buffer.get_unchecked(head..end).into() })
-//         Ok(NonNull::from_raw_parts(ptr.cast(), len))
-//     }
-
-//     /// Deallocate given memory block owned by this `Chunk`.
-//     ///
-//     /// # Safety
-//     ///
-//     /// The given `ptr` must be owned by this `Chunk`, i.e. have been previously allocated using
-//     /// `allocate` of the same `Chunk`. The `layout` must be same layout used to allocate.
-//     unsafe fn deallocate(&self, layout: Layout, ptr: NonNull<u8>) {
-//         self.entries.fetch_sub(1, Ordering::SeqCst);
-//     }
-// }
-
-// impl Drop for Chunk {
-//     fn drop(&mut self) {
-//         unsafe {
-//             std::alloc::dealloc(self.ptr.as_ptr(), self.layout);
-//         }
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use std::alloc::{Allocator, Layout};
-//     use std::slice;
-
-//     use super::{Arena, PAGE};
-
-//     #[test]
-//     fn test_arena() {
-//         let arena = Arena::default();
-//         let ptr = arena.allocate(Layout::new::<u8>()).unwrap();
-
-//         unsafe { arena.deallocate(ptr.to_raw_parts().0.cast(), Layout::new::<u8>()) };
-
-//         let mut ptr = arena.allocate(Layout::array::<u8>(PAGE).unwrap()).unwrap();
-//         let slice = unsafe { ptr.as_mut() };
-//         for b in slice {
-//             *b = 25;
-//         }
-
-//         unsafe {
-//             arena.deallocate(
-//                 ptr.to_raw_parts().0.cast(),
-//                 Layout::array::<u8>(PAGE).unwrap(),
-//             );
-//         }
-//     }
-
-//     #[test]
-//     fn test_arena_vec() {
-//         let arena = Arena::default();
-
-//         let mut vec = Vec::new_in(arena);
-//         vec.push("Hello World");
-//         vec.push("str1");
-//         vec.push("str2");
-//         vec.push("str3");
-//         vec.truncate(0);
-//         vec.shrink_to_fit();
-//         drop(vec);
-//     }
-// }
 
 #[cfg(test)]
 mod tests {

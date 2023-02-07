abe27dfe3fe8f4495fa9a68db783f8400a5d8c67
MrGunflame <mrgunflame@protonmail.com>
2023-02-07 22:55:52 +0100

    Add proper argument parsing for the play crate

--- streamsync-play/src/main.rs
+++ streamsync-play/src/main.rs
@@ -1,37 +1,73 @@
 use std::process::Command;
 
+use clap::Parser;
 use streamsync_api::v1::Session;
 use streamsync_api::Client;
 
-#[tokio::main]
-async fn main() {
-    let mut args = std::env::args().skip(1);
+#[derive(Clone, Debug, Parser)]
+#[command(author, version, long_about = None)]
+pub struct Args {
+    #[arg(long, short = 'u', default_value_t = String::from("127.0.0.1"))]
+    pub host: String,
 
-    let id = args.next().unwrap();
-    let token = args.next().unwrap();
+    #[arg(long, default_value_t = 9998)]
+    pub http_port: u16,
+    #[arg(long, default_value_t = String::from("http"))]
+    pub http_scheme: String,
 
-    let client = Client::new("http://127.0.0.1:9998");
-    client.authorize(token);
-    let session = Session::create(&client, id).await.unwrap();
+    #[arg(long, default_value_t = 9999)]
+    pub srt_port: u16,
 
-    tokio::task::spawn(async move {
-        tokio::signal::ctrl_c().await.unwrap();
-        std::process::exit(0);
-    });
-
-    ffplay(&session.resource_id, &session.session_id);
+    #[arg(long, short = 'r')]
+    pub resource: String,
+    #[arg(long, short = 't')]
+    pub token: String,
 }
 
-pub fn ffplay(r: &str, s: &str) {
-    let addr = format!(
-        "srt://127.0.0.1:9999?streamid=#!::m=request,r={},s={}",
-        r, s
-    );
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
 
-    let cmd = Command::new("ffplay")
-        .args(["-fflags", "nobuffer"])
-        .args(["-flags", "low_delay"])
-        .arg(addr)
-        .spawn()
-        .unwrap();
+    let client = Client::new(format!(
+        "{}://{}:{}",
+        args.http_scheme, args.host, args.http_port
+    ));
+    client.authorize(args.token);
+
+    let res = Session::create(&client, args.resource).await.unwrap();
+
+    let opts = SrtOptions {
+        host: args.host,
+        port: args.srt_port,
+        resource_id: res.resource_id,
+        session_id: res.session_id,
+    };
+
+    ffplay(&opts);
+}
+
+#[derive(Clone, Debug)]
+pub struct SrtOptions {
+    pub host: String,
+    pub port: u16,
+    pub resource_id: String,
+    pub session_id: String,
+}
+
+impl SrtOptions {
+    pub fn address(&self) -> String {
+        format!(
+            "srt://{}:{}?streamid=#!::m=request,r={},s={}",
+            self.host, self.port, self.resource_id, self.session_id
+        )
+    }
+}
+
+pub fn ffplay(opts: &SrtOptions) {
+    let addr = opts.address();
+
+    let mut cmd = Command::new("ffplay");
+    cmd.args(["-fflags", "nobuffer", "-flags", "low_delay", &addr]);
+
+    cmd.spawn().unwrap().wait().unwrap();
 }
